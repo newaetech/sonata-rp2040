1ad5dc49e9dc74583132d26bd1d9fbabe42ecb3e
Alex Dewar <alexldewar@gmail.com>
2024-04-15 18:22:35 -0300

    Linux test fixes
    Should figure out a better way to get the directory for sonata

--- tests/test_linux.py
+++ tests/test_linux.py
@@ -2,21 +2,23 @@ import numpy as np
 from time import sleep
 from test_common import *
 import logging
+import os
 logging.basicConfig(filename="log.txt", level=logging.DEBUG, filemode='w', 
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',)
 
-sonata_path = "~/sonata"
+sonata_path = "../../sonata" #TODO: make this better
 print("Copying sonata.bit...")
 copy_sonata_bitstream(sonata_path)
+os.sync() #ensure above write finishes
 
 print ("Done copy. Testing config write and parse...")
 
 write_all_options(sonata_path)
+os.sync() #ensure above write finishes
 
 print("Done. Ejecting drive...")
 sleep(0.25)
-win_eject_drive()
 
 results, full_file = get_test_results(sonata_path)
 intended_failures = (3, 4) # these ones check to make sure invalid 
